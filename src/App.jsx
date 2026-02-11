7e4e37a6f1c94d04feff8db40824d1120bb42797
DirushanDev <rubakumardirushan@gmail.com>
2026-02-11 11:12:29 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <Navbar />
+       
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
